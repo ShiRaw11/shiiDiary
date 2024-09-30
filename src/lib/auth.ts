2a19a62e91b8ceb8a82bf4bb4bccd568e8e75843
ShiRaw11 <shiikimani11@gmail.com>
2024-09-30 21:48:59 +0300

    redirect

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ export const getUser = async ()=>{
 const auth = getSupabaseAuth()
 const user = (await auth.getUser()).data.user
 
-if (!user) redirect("/login")
+if (!user) redirect("/")
 
     return user
 }
