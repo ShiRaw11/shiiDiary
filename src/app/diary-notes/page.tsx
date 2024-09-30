f2dac9263a3d2b8e6bfb788db14441424cb50aec
ShiRaw11 <shiikimani11@gmail.com>
2024-09-30 20:55:14 +0300

    remove tsparticles

--- src/app/diary-notes/page.tsx
+++ src/app/diary-notes/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/header";
 import Note from "@/components/note";
-import { ParticleBackground } from "@/components/particleBackground";
 import db from "@/db";
 
 import { Notes } from "@/db/schemas/notes";
@@ -12,7 +11,7 @@ export default async function Home() {
 const _notes = await db.select().from(Notes).where(eq(Notes.userId, user.id)).orderBy(desc(Notes.updatedAt))
   return (
     <main className="flex relative z-10 min-h-screen flex-col items-center px-4 pb-24">
-      <ParticleBackground/>
+      
     <Header />
     <div className="mt-8 grid w-full max-w-[1800px] grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 {_notes.map((note)=>
